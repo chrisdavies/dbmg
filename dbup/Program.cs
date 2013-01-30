4134fc65fd536988fadc5a98284bfe2958b84152
Chris <chris.davies@sparcedge.com>
2013-01-30 14:02:34 -0500

    Initial commit.

---
+++ dbup/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DBUp
+{
+    using DBUp.Properties;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine(Resources.Instructions);
+        }
+    }
+}
---
+++ dbup/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dbup")]
+[assembly: AssemblyDescription("Super simple database migrations for .net")]
+[assembly: AssemblyCompany("chrisdavies")]
+[assembly: AssemblyProduct("dbup")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb2eff65-6e16-4ffc-aa2b-e13e953c7140")]
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
