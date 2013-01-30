e3821706f0c0bfa6460eb8d0c623859fb82bd4f2
Chris <chris.davies@sparcedge.com>
2013-01-30 16:44:55 -0500

    Displaying help when invalid commandline args are specified.

--- dbup/Program.cs
+++ dbup/Program.cs
@@ -14,6 +14,7 @@
             catch (ArgumentException ex)
             {
                 Logger.WriteLine(ConsoleColor.Red, ex.Message);
+                Help();
             }
         }
         
