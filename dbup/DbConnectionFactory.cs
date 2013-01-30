467c6e838a5c835f9380835c15ac7e476ba2267b
Chris <chris.davies@sparcedge.com>
2013-01-30 14:40:17 -0500

    Adding support for mysql, mssql, and postgres

---
+++ dbup/DbConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace DBUp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+
+    public class DbConnectionFactory
+    {
+        public static IDbConnection Load(string name)
+        {
+            var providers = new Dictionary<string, Func<IDbConnection>>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "postgres", () => new Npgsql.NpgsqlConnection() },
+                { "mysql", () => new MySql.Data.MySqlClient.MySqlConnection() },
+                { "mssql", () => new System.Data.SqlClient.SqlConnection() }
+            };
+
+            Func<IDbConnection> getConnection;
+
+            if (!providers.TryGetValue(name, out getConnection))
+            {
+                throw new ArgumentException(string.Format("Couldn't find db provider {0}.", name));
+            }
+
+            return getConnection();
+        }
+    }
+}
